823ca6f17d88db526db27c9551ec3903f3523dea
Crowdbotics <team@crowdbotics.com>
2019-06-26 12:11:35 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.mide_shopping_mall_5223.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/mide_shopping_mall_5223/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mide_shopping_mall_5223 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mide_shopping_mall_5223.settings")
+
+application = get_wsgi_application()
